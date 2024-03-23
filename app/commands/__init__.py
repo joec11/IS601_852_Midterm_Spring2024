c2c638260284c4817e0e2431645551629d9a3c92
joec11 <jc625@njit.edu>
2024-03-23 16:06:59 -0400

    Created app/commands/__init__.py to register commands and contain the execute_command method to return the result of the execute method of a command.

---
+++ app/commands/__init__.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self): \
+        pass
+
+class CommandHandler:
+    def __init__(self): # Constructor
+        self.commands = {}
+
+    def register_command(self, command_name: str, command: Command):
+        """Register Command"""
+        self.commands[command_name] = command
+
+    def execute_command(self, *args):
+        """Execute Command"""
+        return self.commands[args[-1]].execute(*args)
