cebdbe4d3e2f2906fb53e51dcbe2e0a95aef5d04
joec11 <jc625@njit.edu>
2024-03-26 14:21:51 -0400

    Modified app/plugins/csv/__init__.py to contain an options dictionary in the class constructor where the user input options, clear, delete, done, and history are the keys and the methods, __clear_option, __delete_option, __done_option, and __history_option are the values corresponding to the keys. Created a __perform private method that calls the corresponding method based on the user input options, clear, delete, done, and history. Modified the following methods to contain private access identifiers, clear_option to __clear_option, delete_option to __delete_option, done_option to __done_option, history_option to __history_option, set_data_path to __set_data_path, and add_to_history to __add_to_history. Changed the if statement to an assertion in the execute public method to check if the data directory (data_dir) and the data file (csv_file_path) is writable. Added a line of code in the execute public method that starts the row index of the pandas dataframe (df_calculations) at one (1) instead of zero (0). Modified the execute public method to call the __perform private method to call the corresponding method based on the user input options, clear, delete, done, and history, and removed the match block and case statements from the REPL loop.

--- app/plugins/csv/__init__.py
+++ app/plugins/csv/__init__.py
@@ -12,29 +12,42 @@ class CsvCommand(Command):
         self.data_dir = None
         self.csv_file_path = None
 
-    def clear_option(self):
+        self.options = {
+            'clear': self.__clear_option,
+            'delete': self.__delete_option,
+            'done': self.__done_option,
+            'history': self.__history_option
+        }
+
+    def __clear_option(self, *args):
         if input("Are you sure you want to clear all records? (Y/N) ").lower() == 'y': \
             self.df_calculations.drop(self.df_calculations.index, inplace=True); \
             logging.info("All records have been deleted from the calculation history."); \
             print("All records have been deleted from the calculation history."); \
             self.df_calculations.to_csv(self.csv_file_path, header=False, index=False)
 
-    def delete_option(self, *args):
+    def __delete_option(self, *args):
         if self.df_calculations.index.isin(*args).any(): \
             self.df_calculations.drop(*args, axis=0, inplace=True); \
             logging.info("Records deleted from the calculation history: %s", *args); \
             print("Records deleted from the calculation history:", *args); \
             self.df_calculations.to_csv(self.csv_file_path, header=False, index=False)
 
-    def done_option(self):
+    def __done_option(self, *args):
         self.done = True; \
         logging.info("Date file management ended."); \
         print("Returned back to main menu.")
 
-    def history_option(self):
+    def __history_option(self, *args):
         print("History is blank." if self.df_calculations.empty else self.df_calculations)
 
-    def set_data_path(self):
+    def __perform(self, option: str, *args):
+        try:
+            self.options[option](*args)  # Call the corresponding method
+        except Exception:
+            print(f"Invalid: {option}")
+
+    def __set_data_path(self):
         load_dotenv(find_dotenv())
         self.data_dir = os.path.abspath(os.getenv('DATA_DIR', 'data')); \
         self.csv_file_path = os.path.join(self.data_dir, os.getenv('CALC_HISTORY_CSV', 'calculations_history.csv'))
@@ -42,49 +55,38 @@ class CsvCommand(Command):
         os.makedirs(self.data_dir, exist_ok=True); \
         open(self.csv_file_path, "w").close() if not os.path.exists(self.csv_file_path) else None
 
-    def add_to_history(self, *args):
+    def __add_to_history(self, *args):
         pd.DataFrame({'A': [args[0]], 'B': [args[1]], 'CMD': [args[2]], 'Result': [args[3]]}) \
             .to_csv(self.csv_file_path, mode='a', header=False, index=False)
 
     def execute(self, *args):
         try:
-            self.set_data_path()
-
-            if not os.access(self.data_dir, os.W_OK) or not os.access(self.csv_file_path, os.W_OK): \
-                raise OSError
+            self.__set_data_path()
+            assert os.access(self.data_dir, os.W_OK) and os.access(self.csv_file_path, os.W_OK) == True
 
             if len(args) == 5: \
-                self.add_to_history(*args); \
+                self.__add_to_history(*args); \
                 return
 
-            self.df_calculations = pd.read_csv(self.csv_file_path, header=None, names=['1st Operand', '2nd Operand', 'Command', 'Result'])
+            self.df_calculations = pd.read_csv(self.csv_file_path, header=None, names=['1st Operand', '2nd Operand', 'Command', 'Result']); \
+            self.df_calculations.index = self.df_calculations.index + 1
 
             logging.info("Date file management started.")
             print("Type 'history' for a history of calculations.\
                 \nType 'delete' followed by the row number(s) to delete specific row(s).\
                 \nType 'clear' to clear the entire history of calculations.\
                 \nType 'done' to exit calculation history management.\n")
-            self.history_option()
+            self.__perform("history")
 
             while not self.done:
                 option_input = input(">>>>>> ").strip().split()
                 try:
-                    assert 0 < len(option_input) <= len(self.df_calculations.index) + 1
+                    assert 0 < len(option_input) <= len(self.df_calculations.index)
 
                     option = option_input[0].lower(); \
                     rows_to_delete = [int(x) for x in option_input[1:]] if (len(option_input) > 1 and option == 'delete') else []
 
-                    match option:
-                        case "clear":
-                            self.clear_option()
-                        case "delete":
-                            self.delete_option(rows_to_delete)
-                        case "done":
-                            self.done_option()
-                        case "history":
-                            self.history_option()
-                        case _:
-                            pass
+                    self.__perform(option, rows_to_delete)
                 except AssertionError:
                     logging.error("Usage: <option> | delete [<row-number(s)>]"); \
                     print("Usage: <option> | delete [<row-number(s)>]")
@@ -92,5 +94,5 @@ class CsvCommand(Command):
                     logging.error(f"Invalid literal(s) - delete can only be followed by integer(s)"); \
                     print(f"Invalid literal(s) - delete can only be followed by integer(s)")
             self.done = False
-        except OSError:
+        except AssertionError:
             logging.error(f"Either the data directory '{self.data_dir}' or the data file '{self.csv_file_path}' is not writable.")
